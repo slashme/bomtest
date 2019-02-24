b6dabef081e1703e0cc60a60bc4876b9baa008b3
David Richfield <davidrichfield@gmail.com>
2019-02-24 13:11:35 +0100

    comment out example

--- hello/views.py
+++ hello/views.py
@@ -9,11 +9,11 @@ def index(request):
     return render(request, "index.html")
 
 
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+#def db(request):
+#
+#    greeting = Greeting()
+#    greeting.save()
+#
+#    greetings = Greeting.objects.all()
+#
+#    return render(request, "db.html", {"greetings": greetings})
